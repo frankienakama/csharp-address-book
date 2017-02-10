f71730d03878fa4374014a360a8c6cd3d9cd9878
Sara <sara.frandsen@gmail.com>
2017-02-10 15:09:42 -0800

    rewrote every page

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,17 +11,20 @@ namespace AddressBook
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
-      Post["/contacts/clear"] = _ => {
-        Contact.ClearAll();
-        return View["cleared.cshtml"];
-      };
-      Get["/contact/add"] = _ => {
-        Contact newContact = new Contact(Request.Form["add-contact"]), Request.Form["add-phonenumber"], Request.Form["add-address"]);
+      Post["/contact/add"] = _ => {
+        Contact newContact = new Contact(Request.Form["add-contact"]), Request.Form["add-phoneNumber"], Request.Form["add-address"]);
         return View["add_contact.cshtml"];
       };
+      Get["/new-contact"] = _ =>{
+        return View["new_contact_confirmation.cshtml"];
+      }
       Get["/contacts/{id}"] = parameters => {
         Contact newContact = Contact.Find(parameters.id);
         return View["contact_info.cshtml", contact];
+      };
+      Post["/contacts/clear"] = _ => {
+        Contact.ClearAll();
+        return View["cleared.cshtml"];
       };
     }
   }
