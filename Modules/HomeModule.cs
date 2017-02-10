e6d0bef19f03429c7fc94e0a31c2414ca7e54568
Sara <sara.frandsen@gmail.com>
2017-02-10 15:58:25 -0800

    finally add some functionality to the add contact forms

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,21 +10,18 @@ namespace AddressBook
     {
       Get["/"] = _ => {
         List<Contact> allContacts = Contact.GetAll();
-        return View["index.cshtml"];
+        return View["index.cshtml", allContacts];
       };
       Get["/contact/add"] = _ => {
         return View["add_contact.cshtml"];
       };
-      Post["/contact/new"] = _ => {
+      Post["/contact/add"] = _ => {
         Contact newContact = new Contact(Request.Form["add-contact"], Request.Form["add-phoneNumber"], Request.Form["add-address"]);
-        return View["new_contact_confirmation.cshtml"];
-      };
-      Get["/contact/new"] = _ =>{
-        return View["new_contact_confirmation.cshtml"];
+        return View["new_contact_confirmation.cshtml", newContact];
       };
       Get["/contacts/{id}"] = parameters => {
-        Contact newContact = Contact.Find(parameters.id);
-        return View["contact_info.cshtml", newContact];
+        Contact contact = Contact.Find(parameters.id);
+        return View["contact_info.cshtml", contact];
       };
       Post["/contacts/clear"] = _ => {
         Contact.ClearAll();
