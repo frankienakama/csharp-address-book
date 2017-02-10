a958e075f5a57ae242c166a418d581055ba448dd
Sara <sara.frandsen@gmail.com>
2017-02-10 13:57:27 -0800

    rewrite ContactInfo.cs

--- Objects/ContactInfo.cs
+++ Objects/ContactInfo.cs
@@ -7,49 +7,63 @@ namespace AddressBook.Objects
 {
   public class ContactInfo
   {
-    private static List<ContactInfo> _instances = new List<ContactInfo> {};
     private string _contactName;
     private string _contactPhoneNumber;
     private string _contactAddress;
     private int _id;
-    private static List<ContactInfo> _contactInfo = new List<ContactInfo>{};
+    private static List<Contact> _instances = new List<Contact>{};
 
-    public List<ContactInfo>(string contactName, string contactPhoneNumber, string contactAddress)
+    public Contact (string contactName, string contactPhoneNumber, string contactAddress)
     {
       _contactName = contactName;
       _contactPhoneNumber = contactPhoneNumber;
       _contactAddress = contactAddress;
       _instances.Add(this);
       _id = _instances.Count;
-      _contactInfo = new List<ContactInfo>{};
     }
     public string GetContactName()
     {
       return _contactName;
     }
+
+    public void SetContactName(string newContactName)
+    {
+      _contactName = newContactName;
+    }
     public string GetContactPhoneNumber()
     {
-      return _contactPhoneNumbere;
+      return _contactPhoneNumber;
+    }
+
+    public void SetContactPhoneNumber(string newContactPhoneNumber)
+    {
+      _contactPhoneNumber = newContactPhoneNumber;
     }
     public string GetContactAddress()
     {
       return _contactAddress;
     }
-    public void AddContact(ContactInfo contactInfo)
+
+    public void SetContactAddress(string newContactAddress)
     {
-      _contactInfo.Add(contactInfo);
+      _contactAddress = newContactAddress;
     }
-    public static List<ContactInfo> GetAll()
+    public static List<Contact> GetAll()
     {
       return _instances;
     }
-    public static void Clear()
+    public int GetId()
+    {
+      return _id;
+    }
+    public static void ClearAll()
     {
       _instances.Clear();
     }
-    public static ContactInfo Find(int searchId)
+    public static Contact Find(int searchId)
     {
-      return _instances[searchId-1];
+      return _instances[searchId - 1];
+    }
     }
   }
 }
